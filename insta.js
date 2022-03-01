1de1a731539c53a87cc0e90d79a61ccb7abb7364
Josue <josuejero@hotmail.com>
2022-03-01 12:22:02 -0500

    new ig token

--- insta.js
+++ insta.js
@@ -1,9 +1,9 @@
 var userFeed = new Instafeed({
-    get: 'user',
-    target: "insta",
-    resolution: 'low_resolution',
-    limit: 5,
-    template: '<a href="{{link}}" target="_blank"><img title="{{caption}}" src="{{image}}" /></a>',
-    accessToken: 'IGQVJYU1R6THFTaFJNMHRuSFA5RTl6QWZA1RlBkeXJOT3BoMVBVV1lmNU9xWXB5R3BObzhySFE5QU8tVXBPM09lR29oY042Vi1WS1M0X1RNdThnQ3NkM3B5NzRMaVQ4T0lNRm9NNkJyYXg4N2RhbndOSAZDZD'
-  });
+  get: 'user',
+  target: "insta",
+  resolution: 'low_resolution',
+  limit: 5,
+  template: '<a href="{{link}}" target="_blank"><img title="{{caption}}" src="{{image}}" /></a>',
+  accessToken: 'IGQVJXLVowV2d2cm9wd2p5cXdrUnVmWmdkSjQ4N1NpX3A0SkdnUHRYX3J3aTlPSjBjUTZAYVVNSanVGSVRVNnZA5TjZAYdGl0V3duOGt4Xy11Nk91MUdjY3pyTXk0Q2NnR0xTcy1VNHM1aWFjTzItWllsOAZDZD'
+});
 userFeed.run();
